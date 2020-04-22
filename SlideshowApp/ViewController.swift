ee8fe364394ec90000a9bdb85db75cd8226f1e5b
kohei morioka <spade.mk21@icloud.com>
2020-04-22 12:37:36 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by kohei morioka on 2020/04/22.
+//  Copyright © 2020 kohei morioka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
